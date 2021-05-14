ed64fec0e8dad44eec3df47078d939e7fbe5829e
Dawid Sowiński <sowadawidos@wp.pl>
2021-05-14 12:22:45 +0200

    update newtask, app, task.js

---
+++ js/Task.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+
+export const Task = ({deleteTask}) => {
+    return (
+        <>
+
+        </>
+    )
+}
--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
+import {getTasks} from "./getTasks";
+import {NewTask} from "./NewTask";
+import {Task} from "./Task";
 
 
 
-const App = () => <h1>Hello World</h1>;
+const App = () => {
+    const [task, setTask] = useState([]);
+
+    useEffect(() => {
+        getTasks(setTask);
+    }, [])
+
+    const addTask = task => {
+        setTask(prev => [task, ...prev]);
+    }
+
+    const deleteTask = id => {
+        setTask( prev => prev.filter(el => el.id !== id))
+    }
+
+    return (
+        <>
+            <NewTask addTask={addTask}/>
+            {
+                task.map( el => <Task key={el.id} deleteTask={deleteTask}/>)
+            }
+        </>
+    )
+}
+
+
 
 ReactDOM.render(
     <App/>,
