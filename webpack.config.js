eebac0fb84b1ada38eb1206e8081ad38798eb1b1
Dawid Sowiński <sowadawidos@wp.pl>
2021-05-14 10:22:31 +0200

    added files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, "build")
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    publicPath: "/build/",
+    compress: true,
+    port: 3001,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
